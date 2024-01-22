fd40c62ce78edb91eebd1a99c5d1983042238686
JayvonK <jaykhuth@gmail.com>
2024-01-21 23:06:51 -0800

    setting up data base

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -1,11 +1,14 @@
+using MakingNewDatabase.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace MakingNewDatabase.Data;
 
     public class DataContext : DbContext
     {
-        public DataContext()
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
         {
-            
+
         }
+
+        public DbSet<Shapes> Shapes { get; set; }
     }
