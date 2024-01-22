973f418a75cd7ee1c85b652f0674ffe4787ff629
JayvonK <jaykhuth@gmail.com>
2024-01-21 23:17:48 -0800

    interface

--- Services/IShapesService.cs
+++ Services/IShapesService.cs
@@ -1,6 +1,8 @@
+using MakingNewDatabase.Models;
+
 namespace MakingNewDatabase.Services;
 
     public interface IShapesService
     {
-        
+        List<Shapes> InputShape(string name, string shape);
     }
